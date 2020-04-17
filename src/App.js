9d71f0eaadb12dfc8c84fb3bbe1d50e7b5563da5
Graham Colmer <grahamcolmer@grahams-mbp.home>
2020-04-17 15:27:07 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,123 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useQuery,useMutation } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+const GET_TODOS = gql`
+  query getTodos {
+    todos {
+      id
+      done
+      text
+    }
+  }
+`
+const TOGGLE_TODO = gql`
+  mutation toggleTodos($id: uuid!, $done: Boolean!) {
+    update_todos(where: {id: {_eq: $id}}, _set: {done: $done}) {
+      returning {
+        done
+        id
+        text
+      }
+    }
+  }
+`
+
+const ADD_TODO = gql`
+  mutation addTodo($text: String!) {
+    insert_todos(objects: {text: $text}) {
+      returning {
+        done
+        id
+        text
+      }
+    }
+  }
+`
+
+const DELETE_TODO = gql`
+  mutation deleteTodo($id: uuid!) {
+    delete_todos(where: {id: {_eq: $id}}) {
+      returning {
+        done
+        id
+        text
+      }
+    }
+  }
+`
+
+// list todos
+// add todos
+// toggle todos
+// delete todos
 
 function App() {
+  const [todoText,setTodoText] = React.useState("")
+  const {data, loading, error } = useQuery(GET_TODOS);
+  const [toggleTodo] = useMutation(TOGGLE_TODO);
+  const [addTodo] = useMutation(ADD_TODO, {
+    onCompleted: () => setTodoText('')
+  });
+  const [deleteTodo] = useMutation(DELETE_TODO);
+
+  function handleToggleTodo({id,done}){
+    toggleTodo({variables: { id,done:!done}})
+  }
+
+  function handleAddTodo(event){
+    event.preventDefault();
+    if(!todoText.trim()) return;
+    addTodo({
+      variables: {text: todoText},
+      refetchQueries: [
+        { query: GET_TODOS}
+      ]
+    });
+  }
+
+  function handleDeleteTodo({id}){
+    const isConfirmed = window.confirm("Are you sure you want to delete this todo");
+    if(isConfirmed) deleteTodo({
+      variables: {id},
+      update: cache => {
+        const prevData = cache.readQuery({ query: GET_TODOS});
+        const newTodos = prevData.todos.filter(todo => todo.id !== id);
+        cache.writeQuery({ query: GET_TODOS, data: { todos: newTodos}});
+      }
+    })
+  }
+
+  if(loading) return <div>Loading...</div>
+  if(error) return <div>Error fetching todos</div>
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="vh-100 code flex flex-column items-center bg-purple white pa3 fl-1">
+      <h1 className="f2-l">GraphQL Checklist</h1>
+      <form className="mb3" onSubmit={handleAddTodo}>
+        <input 
+          className="pa2 f4 b--dashed" 
+          type="text" 
+          placeholder="Enter todo" 
+          onChange={event => setTodoText(event.target.value)} 
+          value={todoText}
+          />
+        <button className="pa2 f4 bg-green" type="submit">Create</button>
+      </form>
+      <div className="flex items-center justify-center flex-column">
+      {data.todos.map(todo => (
+        <p onDoubleClick={() => handleToggleTodo(todo)} key={todo.id}>
+          <span className={`pointer list pa1 f3 ${todo.done && 'strike'}`}>{todo.text}</span>
+          <button onClick={() => handleDeleteTodo(todo)} className="bg-transparent nt bn f4">
+            <span className="red">
+            &times;
+            </span>
+          </button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      ))}
+      </div>
     </div>
   );
 }
